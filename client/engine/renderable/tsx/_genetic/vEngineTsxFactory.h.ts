e8ab66b62f5a270606bfae739b6d694e9b577f19
Kozlov Victor <kozlovvictorzx@gmail.com>
2024-10-18 23:56:50 +0300

    MC-01: docx support

--- client/engine/renderable/tsx/_genetic/vEngineTsxFactory.h.ts
+++ client/engine/renderable/tsx/_genetic/vEngineTsxFactory.h.ts
@@ -5,6 +5,10 @@ import {BaseTsxComponent} from "@engine/renderable/tsx/base/baseTsxComponent";
 const flattenDeep = <T>(arr:(T[]|T)[]):T[]=> {
     const res =  arr.reduce((acc, val) => {
         if (Array.isArray(val)) {
+            // val.forEach((v,i)=>{
+            //     if (v instanceof VirtualNode) {
+            //     }
+            // });
             return (acc as T[]).concat(flattenDeep(val as T[]));
         } else return (acc as T[]).concat(val as T);
     },[]);
